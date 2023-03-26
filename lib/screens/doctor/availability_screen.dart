0c33c5783c1d7759962822a6dc2e3802fc6ffd76
Ali Zoubair <88006585+alizoubair@users.noreply.github.com>
2023-03-26 04:38:02 +0000

    added doctor availability screen

---
+++ lib/screens/doctor/availability_screen.dart
@@ -0,0 +1,197 @@
+import 'package:mydoc/components/button.dart';
+import 'package:mydoc/components/custom_appbar.dart';
+import 'package:mydoc/main.dart';
+import 'package:mydoc/models/booking_datetime_converted.dart';
+import 'package:mydoc/providers/dio_provider.dart';
+import 'package:mydoc/providers/utils.dart';
+import 'package:mydoc/utils/config.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class AvailabilityScreen extends StatefulWidget {
+  const AvailabilityScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AvailabilityScreen> createState() => _AvailabilityScreen();
+}
+
+class _AvailabilityScreen extends State<AvailabilityScreen> {
+  //declaration
+  CalendarFormat _format = CalendarFormat.month;
+  DateTime _focusDay = DateTime.now();
+  DateTime _currentDay = DateTime.now();
+  int? _currentIndex;
+  bool _isWeekend = false;
+  bool _dateSelected = false;
+  bool _timeSelected = false;
+  String? token; //get token for insert booking date and time into database
+
+  Future<void> getToken() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    token = prefs.getString('token') ?? '';
+  }
+
+  @override
+  void initState() {
+    getToken();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Config().init(context);
+    final doctor = ModalRoute.of(context)!.settings.arguments as Map;
+    return Scaffold(
+      appBar: const CustomAppBar(
+        appTitle: 'Availability',
+        icon: FaIcon(Icons.arrow_back_ios),
+      ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverToBoxAdapter(
+            child: Column(
+              children: <Widget>[
+                _tableCalendar(),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 25),
+                  child: Center(
+                    child: Text(
+                      'Select Unavailability Time',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          _isWeekend
+              ? SliverToBoxAdapter(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 30),
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'Weekend is already unavailable, please select another date',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                )
+              : SliverGrid(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      return InkWell(
+                        splashColor: Colors.transparent,
+                        onTap: () {
+                          setState(() {
+                            _currentIndex = index;
+                            _timeSelected = true;
+                          });
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: _currentIndex == index
+                                  ? Colors.white
+                                  : Colors.black,
+                            ),
+                            borderRadius: BorderRadius.circular(15),
+                            color: _currentIndex == index
+                                ? Config.primaryColor
+                                : null,
+                          ),
+                          alignment: Alignment.center,
+                          child: Text(
+                            '${index + 9}:00 ${index + 9 > 11 ? "PM" : "AM"}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color:
+                                  _currentIndex == index ? Colors.white : null,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    childCount: 8,
+                  ),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4, childAspectRatio: 1.5),
+                ),
+          SliverToBoxAdapter(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 80),
+              child: Button(
+                width: double.infinity,
+                title: 'Validate availability',
+                onPressed: () async {
+                  //convert date/day/time into string first
+                  final getDate = DateConverted.getDate(_currentDay);
+                  final getDay = DateConverted.getDay(_currentDay.weekday);
+                  final getTime = DateConverted.getTime(_currentIndex!);
+
+                  final res = await DioProvider().storeAvailability(
+                      getDate, getDay, getTime, doctor['doctor_id'], token!);
+                  
+                  // redirect to doctor home page upon 200 status code
+                  if (res == 200) {
+                    MyApp.navigatorKey.currentState!.pushNamed('drhome');
+                  }
+                },
+                disable: _timeSelected && _dateSelected ? false : true,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  //table calendar
+  Widget _tableCalendar() {
+    return TableCalendar(
+      focusedDay: _focusDay,
+      firstDay: DateTime.now(),
+      lastDay: DateTime(2023, 12, 31),
+      calendarFormat: _format,
+      currentDay: _currentDay,
+      rowHeight: 48,
+      calendarStyle: const CalendarStyle(
+        todayDecoration:
+            BoxDecoration(color: Config.primaryColor, shape: BoxShape.circle),
+      ),
+      availableCalendarFormats: const {
+        CalendarFormat.month: 'Month',
+      },
+      onFormatChanged: (format) {
+        setState(() {
+          _format = format;
+        });
+      },
+      onDaySelected: ((selectedDay, focusedDay) {
+        setState(() {
+          _currentDay = selectedDay;
+          _focusDay = focusedDay;
+          _dateSelected = true;
+
+          //check if weekend is selected
+          if (selectedDay.weekday == 6 || selectedDay.weekday == 7) {
+            _isWeekend = true;
+            _timeSelected = false;
+            _currentIndex = null;
+          } else {
+            _isWeekend = false;
+          }
+        });
+      }),
+    );
+  }
+}
