e780585e6c314a370b1d2b7ae254bc00ea6d9ab3
SigFaulter <SigFaulter@github.com>
2023-03-26 12:42:04 +0000

    fix

--- lib/screens/doctor/home_screen.dart
+++ lib/screens/doctor/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mydoc/screens/patient/appointment_screen.dart';
+import 'package:mydoc/providers/dio_provider.dart';
 
 class DrHomeScreen extends StatelessWidget {
   const DrHomeScreen({super.key});
@@ -7,91 +7,123 @@ class DrHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-        child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Row(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: const [
-              Text(
-                "Hello Dr Alex",
-                style: TextStyle(
-                  fontSize: 35,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
+              Header(),
               CircleAvatar(
                 radius: 25,
                 backgroundImage: AssetImage("images/doctor1.jpg"),
               )
-            ],
+            ]),
+      ),
+      const SizedBox(height: 15),
+      const Padding(
+        padding: EdgeInsets.only(left: 15),
+        child: Text(
+          "List of patients",
+          style: TextStyle(
+            fontSize: 23,
+            fontWeight: FontWeight.w500,
+            color: Colors.black54,
           ),
         ),
-        const SizedBox(height: 15),
-        const Padding(
-          padding: EdgeInsets.only(left: 15),
-          child: Text(
-            "List of patients",
-            style: TextStyle(
-              fontSize: 23,
-              fontWeight: FontWeight.w500,
-              color: Colors.black54,
-            ),
-          ),
-        ),
-        GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          itemCount: 4,
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemBuilder: (context, index) {
-            return InkWell(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AppointmentScreen(),
-                    ));
-              },
-              child: Container(
-                margin: const EdgeInsets.all(10),
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 4,
-                        spreadRadius: 2,
-                      )
-                    ]),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
-                    CircleAvatar(
-                      radius: 35,
-                      backgroundImage: AssetImage("images/doctor1.png"),
-                    ),
-                    Text(
-                      "Ali Zoubair",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.black54,
-                      ),
-                    ),
-                  ],
-                ),
+      ),
+      const PatientsBuilder()
+    ]));
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: DioProvider().getCurrentDoctor(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text('Loading...');
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return Text(
+              "Hello Dr. $snapshot.data['full_name']",
+              style: const TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.w500,
               ),
             );
-          },
-        ),
-      ],
-    ));
+          }
+        });
+  }
+}
+
+class PatientsBuilder extends StatelessWidget {
+  const PatientsBuilder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: DioProvider().getPatients(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text('Loading...');
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              shrinkWrap: true,
+              itemCount: snapshot.data.length,
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    // TODO show patient details
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 4,
+                            spreadRadius: 2,
+                          )
+                        ]),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const CircleAvatar(
+                          radius: 35,
+                          backgroundImage: AssetImage("images/doctor1.png"),
+                        ),
+                        Text(
+                          "${snapshot.data[index]['full_name']}",
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        });
   }
 }
