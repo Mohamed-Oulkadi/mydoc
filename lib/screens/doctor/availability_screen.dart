152b089d1cdf83de91787147864db58db64c1210
Ali Zoubair <88006585+alizoubair@users.noreply.github.com>
2023-04-06 23:13:51 +0000

    added time picker

--- lib/screens/doctor/availability_screen.dart
+++ lib/screens/doctor/availability_screen.dart
@@ -1,3 +1,8 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:get/get.dart';
 import 'package:mydoc/components/button.dart';
 import 'package:mydoc/components/custom_appbar.dart';
 import 'package:mydoc/main.dart';
@@ -6,7 +11,16 @@ import 'package:mydoc/providers/dio_provider.dart';
 import 'package:mydoc/utils/config.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:time_range_picker/time_range_picker.dart';
+
+var doctor;
+
+void fetchData() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  doctor = await json.decode(prefs.get('doctor') as String);
+}
 
 class AvailabilityScreen extends StatefulWidget {
   const AvailabilityScreen({Key? key}) : super(key: key);
@@ -20,14 +34,51 @@ class _AvailabilityScreen extends State<AvailabilityScreen> {
   CalendarFormat _format = CalendarFormat.month;
   DateTime _focusDay = DateTime.now();
   DateTime _currentDay = DateTime.now();
-  int? _currentIndex;
   bool _isWeekend = false;
   bool _dateSelected = false;
   bool _timeSelected = false;
+  String? _startTime;
+  String? _endTime;
+  String? _selectedTime;
+
+  Future<void> _show() async {
+    TimeRange? result = await showTimeRangePicker(
+        context: context,
+        start: const TimeOfDay(hour: 9, minute: 0),
+        end: const TimeOfDay(hour: 12, minute: 0),
+        disabledTime: TimeRange(
+            startTime: const TimeOfDay(hour: 22, minute: 0),
+            endTime: const TimeOfDay(hour: 5, minute: 0)),
+        disabledColor: Colors.red.withOpacity(0.5),
+        strokeWidth: 4,
+        ticks: 24,
+        ticksOffset: -7,
+        ticksLength: 15,
+        ticksColor: Colors.grey,
+        labels: [
+          "12 am",
+          "3 am",
+          "6 am",
+          "9 am",
+          "12 pm",
+          "3 pm",
+          "6 pm",
+          "9 pm"
+        ].asMap().entries.map((e) {
+          return ClockLabel.fromIndex(idx: e.key, length: 8, text: e.value);
+        }).toList(),
+        labelOffset: 35,
+        rotateLabels: false,
+        padding: 60);
+
+    _timeSelected = true;
+    _startTime = result?.startTime.format(context);
+    _endTime = result?.endTime.format(context);
+  }
 
   @override
   Widget build(BuildContext context) {
-    Config().init(context);
+    fetchData();
     return Scaffold(
       appBar: const CustomAppBar(
         appTitle: 'Availability',
@@ -39,18 +90,6 @@ class _AvailabilityScreen extends State<AvailabilityScreen> {
             child: Column(
               children: <Widget>[
                 _tableCalendar(),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 25),
-                  child: Center(
-                    child: Text(
-                      'Select Unavailability Time',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                )
               ],
             ),
           ),
@@ -70,62 +109,32 @@ class _AvailabilityScreen extends State<AvailabilityScreen> {
                     ),
                   ),
                 )
-              : SliverGrid(
-                  delegate: SliverChildBuilderDelegate(
-                    (context, index) {
-                      return InkWell(
-                        splashColor: Colors.transparent,
-                        onTap: () {
-                          setState(() {
-                            _currentIndex = index;
-                            _timeSelected = true;
-                          });
-                        },
-                        child: Container(
-                          margin: const EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: _currentIndex == index
-                                  ? Colors.white
-                                  : Colors.black,
-                            ),
-                            borderRadius: BorderRadius.circular(15),
-                            color: _currentIndex == index
-                                ? Config.primaryColor
-                                : null,
-                          ),
-                          alignment: Alignment.center,
-                          child: Text(
-                            '${index + 9}:00 ${index + 9 > 11 ? "PM" : "AM"}',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              color:
-                                  _currentIndex == index ? Colors.white : null,
-                            ),
-                          ),
-                        ),
-                      );
+              : SliverToBoxAdapter(
+                  child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: Button(
+                    width: double.infinity,
+                    title: 'Select Unavailability Time',
+                    onPressed: () async {
+                      _show();
                     },
-                    childCount: 8,
+                    disable: false,
                   ),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 4, childAspectRatio: 1.5),
-                ),
+                )),
           SliverToBoxAdapter(
             child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 80),
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
               child: Button(
                 width: double.infinity,
                 title: 'Validate availability',
                 onPressed: () async {
-                  //convert date/day/time into string first
+                  //convert date into string first
                   final getDate = DateConverted.getDate(_currentDay);
-                  final getDay = DateConverted.getDay(_currentDay.weekday);
-                  final getTime = DateConverted.getTime(_currentIndex!);
                   final res = await DioProvider().storeAvailability(
-                      getDate, getDay, getTime, 1); // TODO Fix doctor id
-                  // redirect to doctor home page upon 200 status code
-                  if (res == 200) {
+                      getDate, _startTime.toString(), _endTime.toString(), doctor['doctor_id']);
+                  // redirect to doctor home page upon 201 status code
+                  if (res == 201) {
                     MyApp.navigatorKey.currentState!.pushNamed('/dr_home');
                   }
                 },
@@ -169,7 +178,6 @@ class _AvailabilityScreen extends State<AvailabilityScreen> {
           if (selectedDay.weekday == 6 || selectedDay.weekday == 7) {
             _isWeekend = true;
             _timeSelected = false;
-            _currentIndex = null;
           } else {
             _isWeekend = false;
           }
