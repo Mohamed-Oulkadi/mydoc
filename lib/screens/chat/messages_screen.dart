e603f2bee05982b9f6321676f2b3fbb3ff8beafb
Ali Zoubair <88006585+alizoubair@users.noreply.github.com>
2023-04-10 00:36:38 +0000

    changed chat list screen

--- lib/screens/chat/messages_screen.dart
+++ lib/screens/chat/messages_screen.dart
@@ -17,103 +17,134 @@ class MessagesScreen extends StatelessWidget {
       "doctor3.jpg",
       "doctor4.jpg",
     ];
-    return SafeArea(
-      minimum: const EdgeInsets.only(top: 50, left: 20, right: 20),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "Messages",
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            const SizedBox(height: 30),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 5),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.black12,
-                      blurRadius: 10,
-                      spreadRadius: 2,
-                    ),
-                  ],
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    // TODO make search work based on doctor name or remove it
-                    SizedBox(
-                      width: 82,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            hintText: "Search",
-                            border: InputBorder.none,
-                          ),
-                        ),
-                      ),
-                    ),
-                    const Icon(
-                      Icons.search,
-                      color: Color(0xFF7165D6),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                "Recent Chat",
+
+    return Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 50),
+              const Text(
+                "Messages",
                 style: TextStyle(
-                  fontSize: 20,
+                  fontSize: 28,
                   fontWeight: FontWeight.w500,
                 ),
               ),
-            ),
-            ListView.builder(
-              physics: const AlwaysScrollableScrollPhysics(),
-              itemCount: 6,
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  minVerticalPadding: 30,
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const ChatScreen(),
-                        ));
-                  },
-                  leading: CircleAvatar(
-                    radius: 30,
-                    backgroundImage: AssetImage(
-                      "images/${imgs[index]}",
-                    ),
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 10,
+                        spreadRadius: 2,
+                      ),
+                    ],
                   ),
-                  title: const Text(
-                    "Dr. Doctor Name",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                    ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  "Recent Chat",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
                   ),
-                );
-              },
-            )
-          ],
+                ),
+              ),
+              ListView.builder(
+                physics: const AlwaysScrollableScrollPhysics(),
+                itemCount: 6,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    minVerticalPadding: 30,
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const ChatScreen(),
+                          ));
+                    },
+                    leading: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage(
+                        "images/${imgs[index]}",
+                      ),
+                    ),
+                    title: const Text(
+                      "Dr. Doctor Name",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => showSearch(
+            context: context,
+            delegate: CustomSearchDelegate(),
+          ),
+          child: const Icon(Icons.search),
+        ));
+  }
+}
+
+class CustomSearchDelegate extends SearchDelegate<String?> {
+  @override
+  String get searchFieldLabel => "Search people";
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = "";
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
       ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text('results');
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestions = [];
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (content, index) => ListTile(
+          leading: Icon(Icons.arrow_left), title: Text(suggestions[index])),
     );
   }
 }
